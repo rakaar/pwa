d1995f3d9d0e41e9a74fd87db07518143af9c99d
rka87338 <rka87338@gmail.com>
2019-08-09 21:27:26 +0530

    Tags in user settings made

--- frontend/src/UserProfile/UserProfile.js
+++ frontend/src/UserProfile/UserProfile.js
@@ -1,44 +1,48 @@
 import React from 'react';
+import SavedArticle from './SavedArticle';
+import SavedTags from './SavedTags';
+
 import '../Styles/UserProfile.scss';
-import SavedArticle from './SavedArticle'
 
 export default function UserProfile() {
   return (
     <div>
-    
-    <div className='profile'> 
-     <div className="name-sec">
-     <img
-        className='user-image'
-        src='https://homepages.cae.wisc.edu/~ece533/images/airplane.png'
-      />
-      <div className="name-r">
-      <h2>John Doe</h2>
-      <a href="#" className="edit-profile">Edit</a>
-      <br></br>
-      <h3>Web Developer, Designer, Machine Learning Enthusiast </h3>
-      
-      <div className='icons'>
-        <i class='fa fa-facebook-square fa-3x' aria-hidden='false' />
-        <i class='fa fa-twitter-square fa-3x' aria-hidden='false' />
-        <i class='fa fa-linkedin-square fa-3x' aria-hidden='false' />
-        <i class='fa fa-medium fa-3x' aria-hidden='false' />
-        <i class='fa fa-github-square fa-3x' aria-hidden='false' />
-      </div>
-      </div>
-     </div>
-    </div>
+      <div className='profile'>
+        <div className='name-sec'>
+          <img
+            className='user-image'
+            src='https://homepages.cae.wisc.edu/~ece533/images/airplane.png'
+          />
+          <div className='name-r'>
+            <h2>John Doe</h2>
+            <a href='#' className='edit-profile'>
+              Edit
+            </a>
+            <br />
+            <h3>Web Developer, Designer, Machine Learning Enthusiast </h3>
 
-    <div className="saved-cards-w">
-      <h2> Saved Articles </h2>
-      <div className="saved-cards-g">
-        <SavedArticle />
-        <SavedArticle />
-        <SavedArticle />
-        <SavedArticle />
+            <div className='icons'>
+              <i class='fa fa-facebook-square fa-3x' aria-hidden='false' />
+              <i class='fa fa-twitter-square fa-3x' aria-hidden='false' />
+              <i class='fa fa-linkedin-square fa-3x' aria-hidden='false' />
+              <i class='fa fa-medium fa-3x' aria-hidden='false' />
+              <i class='fa fa-github-square fa-3x' aria-hidden='false' />
+            </div>
+          </div>
+        </div>
       </div>
-    </div>
 
+      <SavedTags />
+
+      <div className='saved-cards-w'>
+        <h2> Saved Articles </h2>
+        <div className='saved-cards-g'>
+          <SavedArticle />
+          <SavedArticle />
+          <SavedArticle />
+          <SavedArticle />
+        </div>
+      </div>
     </div>
   );
 }
