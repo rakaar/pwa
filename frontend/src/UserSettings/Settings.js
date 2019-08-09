a54246f76a07d9d92d78c0224644f0c9cdeea072
rka87338 <rka87338@gmail.com>
2019-08-09 23:12:13 +0530

    Made Settings page responsive, textarea is used for bio instead of input

--- frontend/src/UserSettings/Settings.js
+++ frontend/src/UserSettings/Settings.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import PropicUploader from './ImageUploader';
 import EditComp from './EditComp';
+import UserBio from './UserBio';
 import '../Styles/UserSettings/Settings.scss';
 import '../App.css';
 
@@ -9,49 +10,48 @@ export default function Settings() {
   let twitter = <i class='fa fa-twitter-square fa-2x' aria-hidden='false' />;
   let medium = <i class='fa fa-medium fa-2x' aria-hidden='false' />;
   let github = <i class='fa fa-github-square fa-2x' aria-hidden='false' />;
-  
+
   const [editStatus, setEditStatus] = useState(true);
 
   return (
-    <div>
+    <div className='settings-page'>
       <h1>Account Settings</h1>
       <div className='image-uploader'>
         <PropicUploader />
       </div>
-      
-      <div className="grid-v-s">
-      <EditComp title='Username' handleLogo='' defaultContent='raghav' />
-      <EditComp
-        title='Bio'
-        handleLogo=''
-        defaultContent='lorem ipsum so and so andos so long'
-      />
-      <EditComp title='Email' handleLogo='' defaultContent='raghav@gmail.com' />
-      <div>
-        <h2 className="social-settings-t">Social </h2>
-      <EditComp
-        title='Facebook Handle'
-        handleLogo={fb}
-        defaultContent='ragh.kau.3'
-      />
-      <EditComp
-        title='Twitter Handle'
-        handleLogo={twitter}
-        defaultContent='ragh.kau.3'
-      />
-      <EditComp
-        title='Medium Handle'
-        handleLogo={medium}
-        defaultContent='ragh.kau.3'
-      />
-      <EditComp
-        title='Github Handle'
-        handleLogo={github}
-        defaultContent='ragh.kau.3'
-      />
-      </div>
-      </div>
 
+      <div className='grid-v-s'>
+        <EditComp title='Username' handleLogo='' defaultContent='raghav' />
+        <UserBio />
+        <EditComp
+          title='Email'
+          handleLogo=''
+          defaultContent='raghav@gmail.com'
+        />
+        <div>
+          <h2 className='social-settings-t'>Social </h2>
+          <EditComp
+            title='Facebook Handle'
+            handleLogo={fb}
+            defaultContent='ragh.kau.3'
+          />
+          <EditComp
+            title='Twitter Handle'
+            handleLogo={twitter}
+            defaultContent='ragh.kau.3'
+          />
+          <EditComp
+            title='Medium Handle'
+            handleLogo={medium}
+            defaultContent='ragh.kau.3'
+          />
+          <EditComp
+            title='Github Handle'
+            handleLogo={github}
+            defaultContent='ragh.kau.3'
+          />
+        </div>
+      </div>
     </div>
   );
 }
