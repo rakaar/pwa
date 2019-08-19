398f09fcddafde1e237503c52ad1816d5974608e
Parth Paradkar <parthparadkar3@gmail.com>
2019-08-19 22:34:31 +0530

    Download issue pdf added

--- frontend/src/Issues/IssueCard.js
+++ frontend/src/Issues/IssueCard.js
@@ -1,29 +1,36 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import '../Styles/IssueCard.scss';
 
-export default function IssueCard() {
-  return (
-    <div>
-      <div class='container'>
-        <img
-          src='https://images.pexels.com/photos/2727736/pexels-photo-2727736.jpeg?cs=srgb&dl=beautiful-brunette-fashion-2727736.jpg&fm=jpg'
-          class='image'
-        />
-
-        <div className='middle'>
-          <h1>January 2018</h1>
-          <br />
-          <div className="bc-i">
-          <a class='button'>
-            See Issue
-          </a>
-          <a class='button'>
-            Download
-          </a>
+class IssueCard extends Component {
+  state = {
+    issue: 'jun19'
+  }
+  render(){
+    return (
+      <div>
+        <div class='container'>
+          <img
+            src='https://images.pexels.com/photos/2727736/pexels-photo-2727736.jpeg?cs=srgb&dl=beautiful-brunette-fashion-2727736.jpg&fm=jpg'
+            class='image'
+          />
+  
+          <div className='middle'>
+            <h1>June 2019</h1>
+            <br />
+            <div className="bc-i">
+            <a class='button'>
+              See Issue
+            </a>
+            <a class='button' href={`https://cdn.iit-techambit.in/issue/${this.state.issue}.pdf`}>
+              Download
+            </a>
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
+
+export default IssueCard
