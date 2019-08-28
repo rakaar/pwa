bd91879bd6bf2cdfb9e0b308914cd8344104db36
Parth Paradkar <parthparadkar3@gmail.com>
2019-08-28 23:13:53 +0530

    IIT KGP section added

--- frontend/src/LandingPage/LandingPage.js
+++ frontend/src/LandingPage/LandingPage.js
@@ -7,7 +7,7 @@ export default function LandingPage() {
 
 
     <div class="section section-header">
-        <div class="parallax filter filter-color-red">
+        <div class="parallax filter filter-color-white">
             <div class="image"
                 style={{backgroundImage: `url('https://cdn.iit-techambit.in/websiteAssets/front.jpg')`}}>
             </div>
@@ -38,180 +38,30 @@ export default function LandingPage() {
     </div>
 
 
-    <div class="section">
-        <div class="container">
-            <div class="row">
-                <div class="title-area">
-                    <h2>IIT Kharagpur invests in IIT Tech Ambit</h2>
-                    <div class="separator separator-danger">✻</div>
-                    <p class="description">We'd like to announce that IIT Kharagpur has made an investment in Tech Ambit, which will help us further our efforts in our mission of making research and tech innovations accessible to India as a whole. Funding amount to be set at 1.2 million rupees.
-</p>
-                </div>
-            </div>
-            <div class="cardlist">
-                <div class="cards">
-                    <div class="row">
-                        <div class="col-md-4">
-                            <Card num="50+" description="Articles"/> 
-                        </div>
-                        <div class="col-md-4">
-                            <Card num={"10+"} description="Issues"/>
-                        </div>
-                        <div class="col-md-4">
-                            <Card num="50+" description="Articles"/>                   
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-
-
-    <div class="section section-our-team-freebie">
-        <div class="parallax filter filter-color-black">
-            <div class="image" style={{backgroundImage:'assets/img/header-2.jpeg'}}>
-            </div>
-            <div class="container">
-                <div class="content">
-                    <div class="row">
-                        <div class="title-area">
-                            <h2>Who We Are</h2>
-                            <div class="separator separator-danger">✻</div>
-                            <p class="description">We promise you a new look and more importantly, a new attitude. We build that by getting to know you, your needs and creating the best looking clothes.</p>
-                        </div>
-                    </div>
-
-                    <div class="team">
-                        <div class="row">
-                            <div class="col-md-10 col-md-offset-1">
-                                <div class="row">
-                                    <div class="col-md-4">
-                                        <div class="card card-member">
-                                            <div class="content">
-                                                <div class="avatar avatar-danger">
-                                                    <img alt="..." class="img-circle" src="assets/img/faces/face_1.jpg"/>
-                                                </div>
-                                                <div class="description">
-                                                    <h3 class="title">Tina</h3>
-                                                    <p class="small-text">CEO / Co-Founder</p>
-                                                    <p class="description">I miss the old Kanye I gotta say at that time I’d like to meet Kanye And I promise the power is in the people and I will use the power given by the people to bring everything I have back to the people.</p>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div class="col-md-4">
-                                        <div class="card card-member">
-                                            <div class="content">
-                                                <div class="avatar avatar-danger">
-                                                    <img alt="..." class="img-circle" src="assets/img/faces/face_4.jpg"/>
-                                                </div>
-                                                <div class="description">
-                                                    <h3 class="title">Andrew</h3>
-                                                    <p class="small-text">Product Designer</p>
-                                                    <p class="description">I miss the old Kanye I gotta say at that time I’d like to meet Kanye And I promise the power is in the people and I will use the power given by the people to bring everything I have back to the people.</p>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div class="col-md-4">
-                                        <div class="card card-member">
-                                            <div class="content">
-                                                <div class="avatar avatar-danger">
-                                                    <img alt="..." class="img-circle" src="assets/img/faces/face_3.jpg"/>
-                                                </div>
-                                                <div class="description">
-                                                    <h3 class="title">Michelle</h3>
-                                                    <p class="small-text">Marketing Hacker</p>
-                                                    <p class="description">I miss the old Kanye I gotta say at that time I’d like to meet Kanye And I promise the power is in the people and I will use the power given by the people to bring everything I have back to the people.</p>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-
-
-    <div class="section section-our-clients-freebie">
-        <div class="container">
-            <div class="title-area">
-                <h5 class="subtitle text-gray">Here are some</h5>
-                <h2>Clients Testimonials</h2>
-                <div class="separator separator-danger">∎</div>
-            </div>
-
-            <ul class="nav nav-text" role="tablist">
-                <li class="active">
-                    <a href="#testimonial1" role="tab" data-toggle="tab">
-                        <div class="image-clients">
-                            <img alt="..." class="img-circle" src="assets/img/faces/face_5.jpg"/>
-                        </div>
-                    </a>
-                </li>
-                <li>
-                    <a href="#testimonial2" role="tab" data-toggle="tab">
-                        <div class="image-clients">
-                            <img alt="..." class="img-circle" src="assets/img/faces/face_6.jpg"/>
-                        </div>
-                    </a>
-                </li>
-                <li>
-                    <a href="#testimonial3" role="tab" data-toggle="tab">
-                        <div class="image-clients">
-                            <img alt="..." class="img-circle" src="assets/img/faces/face_2.jpg"/>
-                        </div>
-                    </a>
-                </li>
-            </ul>
-
-
-            <div class="tab-content">
-                <div class="tab-pane active" id="testimonial1">
-                    <p class="description">
-                        And I used a period because contrary to popular belief I strongly dislike exclamation points! We no longer have to be scared of the truth feels good to be home In Roman times the artist would contemplate proportions and colors. Now there is only one important color... Green I even had the pink polo I thought I was Kanye I promise I will never let the people down. I want a better life for all!
-                    </p>
-                </div>
-                <div class="tab-pane" id="testimonial2">
-                    <p class="description">Green I even had the pink polo I thought I was Kanye I promise I will never let the people down. I want a better life for all! And I used a period because contrary to popular belief I strongly dislike exclamation points! We no longer have to be scared of the truth feels good to be home In Roman times the artist would contemplate proportions and colors. Now there is only one important color...
-                    </p>
-                </div>
-                <div class="tab-pane" id="testimonial3">
-                    <p class="description"> I used a period because contrary to popular belief I strongly dislike exclamation points! We no longer have to be scared of the truth feels good to be home In Roman times the artist would contemplate proportions and colors. The 'Gaia' team did a great work while we were collaborating. They provided a vision that was in deep connection with our needs and helped us achieve our goals.
-                    </p>
-                </div>
-
-            </div>
-
-        </div>
-    </div>
-
-
-    {/* <div class="section section-small section-get-started">
-        <div class="parallax filter">
+    <div className="section section-header">
+        <div class="parallax filter filter-color-white">
             <div class="image"
-                style={{backgroundImage: 'assets/img/office-1.jpeg'}}>
+                style={{backgroundImage: `url('https://cdn.iit-techambit.in/websiteAssets/iitkgp.jpg')`}}>
             </div>
-            <div class="container">
-                <div class="title-area">
-                    <h2 class="text-white">Do you want to work with us?</h2>
-                    <div class="separator line-separator">♦</div>
-                    <p class="description"> We are keen on creating a second skin for anyone with a sense of style! We design our clothes having our customers in mind and we never disappoint!</p>
+            <div className="container">
+                <div className="row">
+                    <div className="title-area">
+                        <h2>IIT Kharagpur invests in IIT Tech Ambit</h2>
+                        <div className="separator separator-danger">✻</div>
+                        <p className="description">We'd like to announce that IIT Kharagpur has made an investment in Tech Ambit, which will help us further our efforts in our mission of making research and tech innovations accessible to India as a whole. Funding amount to be set at 1.2 million rupees.</p>
+                    </div>
                 </div>
-
-                <div class="button-get-started">
-                    <a href="#gaia" class="btn btn-danger btn-fill btn-lg">Contact Us</a>
+                <div className='cards'>
+                    <Card />
+                    <Card />
+                    <Card />
                 </div>
-            </div>
         </div>
-    </div> */}
+        
+    </div>
 
 
+</div>
    
         </div>
     )
